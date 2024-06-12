a6ecef3b8e7a2d4768684c1e5436b151850102ff
Ангелина Месяцына <angelmesss13@gmail.com>
2024-06-12 13:36:41 +0300

    step5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,32 +1,21 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import gameBody from '../src/gameBody.js';
 
-const isEven = (n) => n % 2 === 0;
+// const userName = greeting();
 
-const playGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
+const description = 'Answer "yes" if the number is oven, otherwise answer "no"';
 
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let correctAnswerCount = 0;
-  while (correctAnswerCount < 3) {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${randomNumber}`);
+const task = () => {
+  const q = Math.round(Math.random() * 100);
 
-    const userAnswer = readlineSync.question('Your answer: ');
-    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-
-    if (userAnswer === correctAnswer) {
-      console.log('Corect!');
-      correctAnswerCount += 1;
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
-      console.log(`Let's try again, ${userName}`);
-    }
+  let a = '';
+  if (q % 2 === 0) {
+    a = 'yes';
+  } else {
+    a = 'no';
   }
 
-  console.log(`Congratulations, ${userName}`);
+  return [q, a];
 };
 
-playGame();
+gameBody(description, task);
